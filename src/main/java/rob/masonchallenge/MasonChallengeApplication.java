33bbc5a9d63071b8ed76b9e5382f2c4cc4fc1c46
Robert Neumann <rneumann@pivotal.io>
2019-04-21 16:53:12 -0700

    Initial project setup

---
+++ src/main/java/rob/masonchallenge/MasonChallengeApplication.java
@@ -0,0 +1,13 @@
+package rob.masonchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasonChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasonChallengeApplication.class, args);
+	}
+
+}
