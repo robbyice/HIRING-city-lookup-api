7d0c235450732a905d3301d78a2453f2aa55fa98
Robert Neumann <rneumann@pivotal.io>
2019-04-22 12:41:18 -0700

    Change cityMatchResult score to only serialize when populated

--- src/main/java/rob/masonchallenge/city/CityMatchResult.java
+++ src/main/java/rob/masonchallenge/city/CityMatchResult.java
@@ -1,17 +1,19 @@
 package rob.masonchallenge.city;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder(toBuilder = true)
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class CityMatchResult implements Comparable<CityMatchResult> {
     @JsonUnwrapped private final City city;
     @JsonIgnore private final double distance;
 
-    private final double score;
+    private final Double score;
 
     @Override
     public int compareTo(CityMatchResult o) {
